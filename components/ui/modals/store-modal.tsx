0b1c4a749e1b5aa0426cff19d30bd5625a466044
Murilo Benatti <benatti1104@gmail.com>
2023-08-30 22:45:44 -0300

    Form Store-Modal

--- components/ui/modals/store-modal.tsx
+++ components/ui/modals/store-modal.tsx
@@ -1,10 +1,31 @@
 "use client";
 
+import * as z from "zod";
+
+import { zodResolver} from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../modal";
+import { useForm } from "react-hook-form";
+import { Form, FormField } from "../form";
+const formSchema = z.object( {
+    name: z.string().min(1),
+
+});
 
 export const StoreModal = () => {
     const storeModal = useStoreModal ();
+
+    const form = useForm<z.infer<typeof formSchema>> ({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    }) 
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
 
     return (
         <Modal
@@ -13,8 +34,26 @@ export const StoreModal = () => {
           isOpen={storeModal.isOpen} 
           onClose={storeModal.onClose}
         >
-        Future Create Store Form
-    </Modal>
+
+<div className="space-y-4 py-2 pb-4">
+        <Form {...Form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({field}) =>}
+                        >
+
+                    </FormField>
+
+
+            </form>
+
+        </Form>
+
+</div>
+
+       </Modal>
 
 
 
